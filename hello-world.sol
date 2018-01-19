5df1cf94c5152c2181a309247bea1ca4851ebfd6
Mariusz <info@mbachurski.com>
2018-01-19 13:40:48 -0800

    Added visibility 'public' to each function, as contract would not deploy.

--- hello-world.sol
+++ hello-world.sol
@@ -1,16 +1,16 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 contract HelloWorld {
     uint256 counter = 0;
     
-    function increase () {
+    function increase() public {
         counter++;
     }
     
-    function decrease () {
+    function decrease() public {
         counter--;
     }
     
-    function getCounter() constant returns(uint256) {
+    function getCounter() public constant returns(uint256) {
         return counter;
     }
 }
